a30d3ffa70f86e2c8cd8a3a0830a1399740e4c43
rodrigo <xravg.rodrigo@gmail.com>
2022-05-28 15:20:52 -0600

    Adds post and display
    Form creates posts and app can retrieve and display data from db

--- index.js
+++ index.js
@@ -2,40 +2,49 @@ const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-// mongoose.connect('mongodb://localhost:27017/my_database');
+const BlogPost = require('./models/BlogPost');
 
 mongoose.connect('mongodb://localhost:27017/my_database', {
-    family: 4
+	family: 4
 })
-    .then(() => console.log("database conection succesfull"))
-    .catch(err => console.log(err));
+	.then(() => console.log("database conection succesfull"))
+	.catch(err => console.log(err));
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(4000, () => {
-    console.log('app listening on port 4000');
+	console.log('app listening on port 4000');
 })
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+	const blogposts = await BlogPost.find({});
+	res.render('index', { blogposts });
 })
 
 app.get('/about', (req, res) => {
-    res.render('about');
+	res.render('about');
 })
 
 app.get('/contact', (req, res) => {
-    res.render('contact');
+	res.render('contact');
 })
 
-app.get('/post', (req, res) => {
-    res.render('post');
+app.get('/post/:id', async (req, res) => {
+	const blogpost = await BlogPost.findById(req.params.id);
+	res.render('post', { blogpost });
 })
 
 app.get('/posts/new', (req, res) => {
-    res.render('create');
+	res.render('create');
 })
 
+app.post('/posts/store', async (req, res) => {
+	await BlogPost.create(req.body);
+	res.redirect('/');
+})
