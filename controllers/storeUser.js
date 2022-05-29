1727c0ff45e890b5eecae7b06d066ffa58f089ff
rodrigo <xravg.rodrigo@gmail.com>
2022-05-29 17:23:18 -0600

    Adds register validation
    Adds form validation to register page

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -4,6 +4,8 @@ const User = require('../models/User');
 module.exports = (req, res) => {
     User.create(req.body, (err, user) => {
         if (err) {
+            const validationErrors = Object.keys(err.errors).map(key => err.errors[key].message);
+            req.flash('validationErrors', validationErrors);
             return res.redirect('/auth/register');
         }
         res.redirect('/');
