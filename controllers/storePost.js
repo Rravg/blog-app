d0b70a8b732f4ff86a05672577aa20e3287bc64c
rodrigo <xravg.rodrigo@gmail.com>
2022-05-28 21:09:58 -0600

    MVC refactoring

---
+++ controllers/storePost.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const BlogPost = require('../models/BlogPost');
+
+module.exports = (req, res) => {
+    let image = req.files.image;
+    let uploadPath = __dirname + '../public/assets/img' + image.name;
+    image.mv(path.resolve(uploadPath), async (err) => {
+        await BlogPost.create({
+            ...req.body,
+            image: '/assets/img' + image.name
+        });
+        res.redirect('/');
+    })
+}
--- index.js
+++ index.js
@@ -1,24 +1,29 @@
+/**
+ * Dependencies
+ */
 const express = require('express');
-const path = require('path');
-const ejs = require('ejs');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 
-const BlogPost = require('./models/BlogPost');
+/**
+ * Controllers
+ */
+const newPostController = require('./controllers/newPost');
+const homeController = require('./controllers/home');
+const getPostController = require('./controllers/getPost');
+const storePostController = require('./controllers/storePost');
+
+/**
+ * Middleware
+ */
+const validateMiddleWare = require('./middleware/validationMiddleware');
 
 mongoose.connect('mongodb://localhost:27017/my_database', {
 	family: 4
 })
 	.then(() => console.log("database conection succesfull"))
 	.catch(err => console.log(err));
-
-const validateMiddleWare = (req, res, next) => {
-	if (req.files == null || req.body.title == '' || req.body.body == '') {
-		return res.redirect('/posts/new');
-	}
-	next();
-}
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -28,39 +33,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 app.use('/posts/store', validateMiddleWare);
 
+app.get('/', homeController);
+
+app.get('/post/:id', getPostController);
+
+app.get('/posts/new', newPostController);
+
+app.post('/posts/store', storePostController);
+
 app.listen(4000, () => {
 	console.log('app listening on port 4000');
-})
-
-app.get('/', async (req, res) => {
-	const blogposts = await BlogPost.find({});
-	res.render('index', { blogposts });
-})
-
-app.get('/about', (req, res) => {
-	res.render('about');
-})
-
-app.get('/contact', (req, res) => {
-	res.render('contact');
-})
-
-app.get('/post/:id', async (req, res) => {
-	const blogpost = await BlogPost.findById(req.params.id);
-	res.render('post', { blogpost });
-})
-
-app.get('/posts/new', (req, res) => {
-	res.render('create');
-})
-
-app.post('/posts/store', (req, res) => {
-	let image = req.files.image;
-	image.mv(path.resolve(__dirname, 'public/assets/img', image.name), async (error) => {
-		await BlogPost.create({
-			...req.body,
-			image: '/assets/img/' + image.name
-		});
-		res.redirect('/');
-	})
 })
