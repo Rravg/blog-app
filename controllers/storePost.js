e35ca18da5d493ee412cc0809e1a042f4d591110
rodrigo <xravg.rodrigo@gmail.com>
2022-05-31 18:16:29 -0600

    Fix post image
    Get Post image was not working properly, fixed resolving path

--- controllers/storePost.js
+++ controllers/storePost.js
@@ -3,11 +3,10 @@ const BlogPost = require('../models/BlogPost');
 
 module.exports = (req, res) => {
     let image = req.files.image;
-    let uploadPath = __dirname + '../public/assets/img' + image.name;
-    image.mv(path.resolve(uploadPath), async (err) => {
+    image.mv(path.resolve(__dirname, '../public/assets/img/', image.name), async (err) => {
         await BlogPost.create({
             ...req.body,
-            image: '/assets/img' + image.name,
+            image: '/assets/img/' + image.name,
             userid: req.session.userId
         });
         res.redirect('/');
